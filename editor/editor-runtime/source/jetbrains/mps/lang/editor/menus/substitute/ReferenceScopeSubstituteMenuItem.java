caf43f003c2c3547503d7b03374bf0280b670fbc
Radimir.Sorokin <rad.sorokin@gmail.com>
2017-04-14 23:23:58 +0300

    MPS-25879 Introduce `reference actions` substitute menu part - runtime classes

---
+++ editor/editor-runtime/source/jetbrains/mps/lang/editor/menus/substitute/ReferenceScopeSubstituteMenuItem.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2003-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.lang.editor.menus.substitute;
+
+import jetbrains.mps.openapi.editor.EditorContext;
+import jetbrains.mps.smodel.presentation.NodePresentationUtil;
+import jetbrains.mps.smodel.runtime.IconResource;
+import jetbrains.mps.smodel.runtime.IconResourceUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.mps.openapi.language.SAbstractConcept;
+import org.jetbrains.mps.openapi.language.SReferenceLink;
+import org.jetbrains.mps.openapi.model.SNode;
+import org.jetbrains.mps.openapi.model.SNodeAccessUtil;
+
+/**
+ * @author Radimir.Sorokin
+ */
+public class ReferenceScopeSubstituteMenuItem extends DefaultSubstituteMenuItem {
+
+  @NotNull
+  private final SNode myReferent;
+
+  @NotNull
+  private final SReferenceLink myReferenceLink;
+
+  public ReferenceScopeSubstituteMenuItem(@NotNull SAbstractConcept concept, @NotNull SNode parentNode, @Nullable SNode currentChild,
+                                          @NotNull SNode referent, @NotNull SReferenceLink referenceLink,
+                                          @NotNull EditorContext editorContext) {
+    super(concept, parentNode, currentChild, editorContext);
+    myReferent = referent;
+    myReferenceLink = referenceLink;
+  }
+
+  @Nullable
+  @Override
+  public String getMatchingText(@NotNull String pattern) {
+    return NodePresentationUtil.matchingText(myReferent, getParentNode());
+  }
+
+  @Nullable
+  @Override
+  public String getDescriptionText(@NotNull String pattern) {
+    return "^" + NodePresentationUtil.descriptionText(myReferent, getParentNode());
+  }
+
+  @Nullable
+  @Override
+  public IconResource getIcon(@NotNull String pattern) {
+    return IconResourceUtil.getIconResourceForNode(myReferent);
+  }
+
+  @Nullable
+  @Override
+  public SNode createNode(@NotNull String pattern) {
+    SNode node = super.createNode(pattern);
+    SNodeAccessUtil.setReferenceTarget(node, myReferenceLink, myReferent);
+    return node;
+  }
+}
---
+++ editor/editor-runtime/source/jetbrains/mps/lang/editor/menus/substitute/ReferenceScopeSubstituteMenuPart.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2003-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.lang.editor.menus.substitute;
+
+import jetbrains.mps.openapi.editor.menus.substitute.SubstituteMenuContext;
+import jetbrains.mps.openapi.editor.menus.substitute.SubstituteMenuItem;
+import jetbrains.mps.scope.Scope;
+import jetbrains.mps.smodel.constraints.ModelConstraints;
+import jetbrains.mps.util.IterableUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.mps.openapi.language.SConcept;
+import org.jetbrains.mps.openapi.language.SContainmentLink;
+import org.jetbrains.mps.openapi.language.SReferenceLink;
+import org.jetbrains.mps.openapi.model.SNode;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Radimir.Sorokin
+ */
+public class ReferenceScopeSubstituteMenuPart implements SubstituteMenuPart {
+
+  @NotNull
+  private final SReferenceLink myReferenceLink;
+
+  @NotNull
+  private final SConcept myConcept;
+
+  public ReferenceScopeSubstituteMenuPart(@NotNull SConcept concept, @NotNull SReferenceLink referenceLink) {
+    myConcept = concept;
+    myReferenceLink = referenceLink;
+  }
+
+  @NotNull
+  @Override
+  public final List<SubstituteMenuItem> createItems(SubstituteMenuContext context) {
+    SNode parentNode = context.getParentNode();
+    SNode currentTarget = context.getCurrentTargetNode();
+    SContainmentLink link = null;
+    int position = 0;
+    if (currentTarget != null) {
+      link = currentTarget.getContainmentLink();
+      position = IterableUtil.indexOf(parentNode.getChildren(link), currentTarget);
+    }
+    Scope scope = ModelConstraints.getReferenceDescriptor(parentNode, link, position, myReferenceLink).getScope();
+    Iterable<SNode> referents = scope.getAvailableElements(null);
+    List<SubstituteMenuItem> result = new ArrayList<>();
+    for (SNode referent: referents) {
+      result.add(new ReferenceScopeSubstituteMenuItem(myConcept, parentNode, currentTarget, referent, myReferenceLink, context.getEditorContext()));
+    }
+    return result;
+  }
+}
