josefmueller/MPS

commit cf0ac78f5029a8b0489286de5af5bd001658ccc1

Rebuild ide.build

plugins/mps-build/solutions/mpsBuild/models/build.mps
@@ -12013,6 +12013,12 @@
               <ref role="3bR37D" node="1H905DlDUSw" resolve="MPS.OpenAPI" />
             </node>
           </node>
+          <node concept="1SiIV0" id="6woObKLuP__" role="3bR37C">
+            <node concept="3bR9La" id="6woObKLuP_A" role="1SiIV1">
+              <property role="3bR36h" value="false" />
+              <ref role="3bR37D" node="44LXwdzyvTi" resolve="Annotations" />
+            </node>
+          </node>
         </node>
         <node concept="1E0d5M" id="fA2nR_yV4L" role="1E1XAP">
           <ref role="1E0d5P" node="1H905DlDUSw" resolve="MPS.OpenAPI" />
@@ -21068,6 +21074,9 @@
       </node>
       <node concept="m$_yC" id="iTJcDIxtWQ" role="m$_yJ">
         <ref role="m$_y1" node="3nGzrDDNo$o" resolve="svn4idea" />
+      </node>
+      <node concept="m$_yC" id="6woObKLuPAM" role="m$_yJ">
+        <ref role="m$_y1" node="6ifcnI8BosJ" resolve="jetbrains.mps.vcs.core" />
       </node>
     </node>
     <node concept="398rNT" id="1lMM4owFq60" role="1l3spd">
@@ -30754,21 +30763,6 @@
           <property role="3MwjfP" value="Plugin is only needed to be a dependency of jetbrains.mps.vcs plugin" />
         </node>
       </node>
-      <node concept="398BVA" id="I6XuqGYeIG" role="I30fb">
-        <ref role="398BVh" node="6ifcnI8$2ke" resolve="mps_home" />
-        <node concept="2Ry0Ak" id="I6XuqGYeIH" role="iGT6I">
-          <property role="2Ry0Am" value="plugins" />
-          <node concept="2Ry0Ak" id="I6XuqGYeII" role="2Ry0An">
-            <property role="2Ry0Am" value="vcs-core" />
-            <node concept="2Ry0Ak" id="I6XuqGYeIJ" role="2Ry0An">
-              <property role="2Ry0Am" value="META-INF" />
-              <node concept="2Ry0Ak" id="I6XuqGYeIK" role="2Ry0An">
-                <property role="2Ry0Am" value="plugin.xml" />
-              </node>
-            </node>
-          </node>
-        </node>
-      </node>
     </node>
     <node concept="55IIr" id="6ifcnI8$2iS" role="auvoZ">
       <node concept="2Ry0Ak" id="6ifcnI8A4QM" role="iGT6I">
@@ -30818,7 +30812,7 @@
           </node>
           <node concept="28jJK3" id="3SChJ9XyqTP" role="39821P">
             <node concept="398BVA" id="3SChJ9XyqUH" role="28jJRO">
-              <ref role="398BVh" node="6ifcnI8$2ke" />
+              <ref role="398BVh" node="6ifcnI8$2ke" resolve="mps_home" />
               <node concept="2Ry0Ak" id="3SChJ9XyqVB" role="iGT6I">
                 <property role="2Ry0Am" value="plugins" />
                 <node concept="2Ry0Ak" id="3SChJ9XyqVG" role="2Ry0An">

plugins/mps-build/solutions/mpsBuild/source_gen.caches/jetbrains/mps/ide/build/generated
@@ -1,3 +1,3 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<dependencies version="2" modelHash="-eo9aqw1asogjnj3c9l73ea7y8d685l1" />
+<dependencies version="2" modelHash="3qbethrry2r4w1rnf5oar1wqjzkzyrl" />
 

plugins/mps-build/solutions/mpsBuild/source_gen.caches/jetbrains/mps/ide/build/tests/generated
@@ -1,3 +1,3 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<dependencies version="2" modelHash="66gec5w9z7s7p2nf8w5351kywd2bzg7" />
+<dependencies version="2" modelHash="-4t26yipf0tz01v8adwanzv5nrrz0n8f" />
 

build/mpsVcs.xml
@@ -135,6 +135,7 @@
       <plugin path="${artifacts.mpsCore}/mps-core/META-INF/plugin.xml/../.." />
       <plugin path="${artifacts.mpsVcsCore}/git4idea" />
       <plugin path="${artifacts.mpsVcsCore}/svn4idea" />
+      <plugin path="${artifacts.mpsVcsCore}/vcs-core" />
       <library file="${artifacts.mpsBootstrapCore}/languages/baseLanguage/jetbrains.mps.baseLanguage.blTypes.jar" />
       <library file="${artifacts.mpsBootstrapCore}/languages/baseLanguage/jetbrains.mps.baseLanguage.checkedDots.jar" />
       <library file="${artifacts.mpsBootstrapCore}/languages/baseLanguage/jetbrains.mps.baseLanguage.classifiers.jar" />

build/mpsVcsCore.xml
@@ -68,9 +68,21 @@
     </copy>
     <mkdir dir="${build.layout}/vcs-core" />
     <mkdir dir="${build.layout}/vcs-core/META-INF" />
-    <copy todir="${build.layout}/vcs-core/META-INF">
-      <fileset file="${mps_home}/plugins/vcs-core/META-INF/plugin.xml" />
-    </copy>
+    <echoxml file="${build.layout}/vcs-core/META-INF/plugin.xml">
+      <idea-plugin version="2" xmlns:xi="http://www.w3.org/2001/XInclude">
+        <id>jetbrains.mps.vcs.core</id>
+        <name>MPS Vcs Core</name>
+        <description>Plugin is only needed to be a dependency of jetbrains.mps.vcs plugin</description>
+        <version>1.0</version>
+        <depends>jetbrains.mps.core</depends>
+        <depends>git4idea</depends>
+        <depends>svn4idea</depends>
+        
+        <extensions defaultExtensionNs="com.intellij">
+          <mps.LanguageLibrary dir="/languages" />
+        </extensions>
+      </idea-plugin>
+    </echoxml>
     <mkdir dir="${build.layout}/vcs-core/META-INF" />
     <copy todir="${build.layout}/vcs-core/META-INF">
       <fileset file="${mps_home}/plugins/vcs/META-INF/MPSVcs.xml" />

plugins/mps-build/solutions/mpsBuild/models/jetbrains/mps/ide/build/tests.mps
@@ -3496,6 +3496,12 @@
             <ref role="3bR37D" node="6sT62Z6Skja" resolve="UtilSolution" />
           </node>
         </node>
+        <node concept="1SiIV0" id="6woObKLuPBA" role="3bR37C">
+          <node concept="3bR9La" id="6woObKLuPBB" role="1SiIV1">
+            <property role="3bR36h" value="false" />
+            <ref role="3bR37D" node="6sT62Z6Ski$" resolve="BHL5" />
+          </node>
+        </node>
       </node>
       <node concept="1E1JtD" id="6sT62Z6SkiJ" role="2G$12L">
         <property role="BnDLt" value="true" />

plugins/mps-build/solutions/mpsBuild/source_gen/jetbrains/mps/ide/build/mpsVcs.xml
@@ -135,6 +135,7 @@
       <plugin path="${artifacts.mpsCore}/mps-core/META-INF/plugin.xml/../.." />
       <plugin path="${artifacts.mpsVcsCore}/git4idea" />
       <plugin path="${artifacts.mpsVcsCore}/svn4idea" />
+      <plugin path="${artifacts.mpsVcsCore}/vcs-core" />
       <library file="${artifacts.mpsBootstrapCore}/languages/baseLanguage/jetbrains.mps.baseLanguage.blTypes.jar" />
       <library file="${artifacts.mpsBootstrapCore}/languages/baseLanguage/jetbrains.mps.baseLanguage.checkedDots.jar" />
       <library file="${artifacts.mpsBootstrapCore}/languages/baseLanguage/jetbrains.mps.baseLanguage.classifiers.jar" />

plugins/mps-build/solutions/mpsBuild/source_gen/jetbrains/mps/ide/build/mpsVcsCore.xml
@@ -68,9 +68,21 @@
     </copy>
     <mkdir dir="${build.layout}/vcs-core" />
     <mkdir dir="${build.layout}/vcs-core/META-INF" />
-    <copy todir="${build.layout}/vcs-core/META-INF">
-      <fileset file="${mps_home}/plugins/vcs-core/META-INF/plugin.xml" />
-    </copy>
+    <echoxml file="${build.layout}/vcs-core/META-INF/plugin.xml">
+      <idea-plugin version="2" xmlns:xi="http://www.w3.org/2001/XInclude">
+        <id>jetbrains.mps.vcs.core</id>
+        <name>MPS Vcs Core</name>
+        <description>Plugin is only needed to be a dependency of jetbrains.mps.vcs plugin</description>
+        <version>1.0</version>
+        <depends>jetbrains.mps.core</depends>
+        <depends>git4idea</depends>
+        <depends>svn4idea</depends>
+        
+        <extensions defaultExtensionNs="com.intellij">
+          <mps.LanguageLibrary dir="/languages" />
+        </extensions>
+      </idea-plugin>
+    </echoxml>
     <mkdir dir="${build.layout}/vcs-core/META-INF" />
     <copy todir="${build.layout}/vcs-core/META-INF">
       <fileset file="${mps_home}/plugins/vcs/META-INF/MPSVcs.xml" />

commit 5e0b8d7bcf8455cea7505df822a1c2afdc027dd3

platform 172.1909: fix vcs plugin dependencies
Bug was introduced by commit cf0ac78f5029a8b0489286de5af5bd001658ccc1:
Removal of source xml file lead to inlining with ant echo, where property id from BuildMps_IdeaPlugin used instead of name child, which is wrong for git4idea and snv4idea plugins - their plugin IDs are stored in name child instead of id property.

plugins/mps-build/solutions/mpsBuild/models/build.mps
@@ -2,11 +2,11 @@
 <model ref="r:874d959d-e3b4-4d04-b931-ca849af130dd(jetbrains.mps.ide.build)">
   <persistence version="9" />
   <languages>
-    <use id="798100da-4f0a-421a-b991-71f8c50ce5d2" name="jetbrains.mps.build" version="0" />
-    <use id="d5033cee-f632-44b6-b308-89d4fbde34ff" name="jetbrains.mps.build.startup" version="0" />
-    <use id="0cf935df-4699-4e9c-a132-fa109541cba3" name="jetbrains.mps.build.mps" version="3" />
-    <use id="698a8d22-a104-47a0-ba8d-10e3ec237f13" name="jetbrains.mps.build.workflow" version="0" />
-    <use id="479c7a8c-02f9-43b5-9139-d910cb22f298" name="jetbrains.mps.core.xml" version="0" />
+    <use id="798100da-4f0a-421a-b991-71f8c50ce5d2" name="jetbrains.mps.build" version="-1" />
+    <use id="d5033cee-f632-44b6-b308-89d4fbde34ff" name="jetbrains.mps.build.startup" version="-1" />
+    <use id="0cf935df-4699-4e9c-a132-fa109541cba3" name="jetbrains.mps.build.mps" version="-1" />
+    <use id="698a8d22-a104-47a0-ba8d-10e3ec237f13" name="jetbrains.mps.build.workflow" version="-1" />
+    <use id="479c7a8c-02f9-43b5-9139-d910cb22f298" name="jetbrains.mps.core.xml" version="-1" />
   </languages>
   <imports />
   <registry>
@@ -12542,6 +12542,12 @@
             <ref role="3bR37D" node="7Kfy9QB6LfQ" resolve="jetbrains.mps.kernel" />
           </node>
         </node>
+        <node concept="1SiIV0" id="35N3_fQD3Vj" role="3bR37C">
+          <node concept="3bR9La" id="35N3_fQD3Vk" role="1SiIV1">
+            <property role="3bR36h" value="false" />
+            <ref role="3bR37D" node="1H905DlDUSw" resolve="MPS.OpenAPI" />
+          </node>
+        </node>
         <node concept="398BVA" id="6wK_PpNA8GH" role="3LF7KH">
           <ref role="398BVh" node="16Vg0jOco3V" resolve="mps_home" />
           <node concept="2Ry0Ak" id="6wK_PpNA8GI" role="iGT6I">
@@ -12555,12 +12561,6 @@
                 </node>
               </node>
             </node>
-          </node>
-        </node>
-        <node concept="1SiIV0" id="35N3_fQD3Vj" role="3bR37C">
-          <node concept="3bR9La" id="35N3_fQD3Vk" role="1SiIV1">
-            <property role="3bR36h" value="false" />
-            <ref role="3bR37D" node="1H905DlDUSw" resolve="MPS.OpenAPI" />
           </node>
         </node>
       </node>

plugins/mps-build/solutions/mpsBuild/source_gen.caches/jetbrains/mps/ide/build/generated
@@ -1,3 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<dependencies version="2" modelHash="zg7oj477rud1qp1vgaqdr4q9lx8586" />
-