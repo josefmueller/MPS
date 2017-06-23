651588efb3065b7b29da89a792cae580dba0e564
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2017-06-23 13:25:42 +0300

    stub for MigrationProblemHandler in plugin

--- IdeaPlugin/mps-migration/src/jetbrains/mps/migration/idea/PluginMigrationProblemHandler.java
+++ IdeaPlugin/mps-migration/src/jetbrains/mps/migration/idea/PluginMigrationProblemHandler.java
@@ -15,5 +15,35 @@
  */
 package jetbrains.mps.migration.idea;
 
-public class PluginMigrationProblemHandler implements MigrationProblemHandler {
+import com.intellij.openapi.components.AbstractProjectComponent;
+import com.intellij.openapi.project.Project;
+import jetbrains.mps.lang.migration.runtime.base.Problem;
+import jetbrains.mps.migration.global.MigrationProblemHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.mps.openapi.model.SNode;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class PluginMigrationProblemHandler extends AbstractProjectComponent implements MigrationProblemHandler, com.intellij.openapi.components.ApplicationComponent {
+  public PluginMigrationProblemHandler(Project p) {
+    super(p);
+  }
+
+  @Override
+  public void showProblems(Collection<Problem> problems) {
+    throw new UnsupportedOperationException("Showing errors in plugin not supported yet");
+  }
+
+  @Override
+  public void showNodes(Map<String, Set<SNode>> nodesByCategory) {
+    throw new UnsupportedOperationException("Showing nodes in plugin not supported yet");
+  }
+
+  @NotNull
+  @Override
+  public String getComponentName() {
+    return getClass().getSimpleName();
+  }
 }
