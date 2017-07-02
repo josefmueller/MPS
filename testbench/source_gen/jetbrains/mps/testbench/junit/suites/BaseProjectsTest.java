59363048dbf8911d8556ae8458273a015055f044
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2017-07-02 14:58:29 +0300

    correct migration test initialisation

--- testbench/source_gen/jetbrains/mps/testbench/junit/suites/BaseProjectsTest.java
+++ testbench/source_gen/jetbrains/mps/testbench/junit/suites/BaseProjectsTest.java
@@ -22,8 +22,8 @@ import org.junit.After;
 
 @RunWith(value = TeamCityParameterizedRunner.class)
 public class BaseProjectsTest {
-  public static final String MIGRATION_PLUGIN = "migrationAssistant";
-  public static final String MIGRATION_PLUGIN_ID = "jetbrains.mps.ide.migration.assistant";
+  public static final String MIGRATION_PLUGIN = "migration";
+  public static final String MIGRATION_PLUGIN_ID = "jetbrains.mps.ide.migration.workbench";
 
   private static Environment ourEnv;
 
