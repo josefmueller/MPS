2b244e7fd5b457e48aec79735e9a9000d793cdf1
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2017-07-17 16:55:40 +0200

    Proper model access for an MPSTree component of Node Explorer tool

--- plugins/mpsdevkit/source/jetbrains/mps/ide/devkit/components/NodeExplorerComponent.java
+++ plugins/mpsdevkit/source/jetbrains/mps/ide/devkit/components/NodeExplorerComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2015 JetBrains s.r.o.
+ * Copyright 2003-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -95,6 +95,11 @@ public class NodeExplorerComponent extends JPanel implements Disposable {
     @Override
     protected ActionGroup createPopupActionGroup(MPSTreeNode node) {
       return null;
+    }
+
+    @Override
+    protected void runRebuildAction(Runnable rebuildAction, boolean saveExpansion) {
+      super.runRebuildAction(new ModelReadRunnable(myProject.getModelAccess(), rebuildAction), saveExpansion);
     }
 
     @Override
