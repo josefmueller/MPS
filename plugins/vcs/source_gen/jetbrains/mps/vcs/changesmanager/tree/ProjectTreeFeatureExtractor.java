josefmueller/MPS

commit ad50c6c1b9ed6bc8e3c4fca0434342f0296b455e

Drop global MPSModuleRepository access, replace with one from context project. Features that need to walk model structure get chance to do that right (to keep repo along with a feature would be too much). ProjectTreeFeatureExtractor ignores non-accessible nodes the same way other extractors (usages, hierarchy) do.

plugins/vcs/source_gen/jetbrains/mps/vcs/changesmanager/tree/ProjectTreeFeatureExtractor.java
@@ -9,9 +9,6 @@ import jetbrains.mps.ide.ui.tree.MPSTreeNode;
 import org.jetbrains.mps.openapi.model.SNode;
 import jetbrains.mps.ide.ui.tree.MPSTreeNodeEx;
 import org.jetbrains.mps.openapi.model.SNodeReference;
-import org.jetbrains.mps.openapi.model.SNodeUtil;
-import jetbrains.mps.smodel.MPSModuleRepository;
-import jetbrains.mps.smodel.SNodePointer;
 import jetbrains.mps.ide.ui.tree.smodel.SNodeTreeNode;
 import jetbrains.mps.ide.ui.smodel.ConceptTreeNode;
 import jetbrains.mps.vcs.changesmanager.tree.features.NodeFeature;
@@ -19,6 +16,7 @@ import jetbrains.mps.ide.ui.smodel.PropertyTreeNode;
 import jetbrains.mps.vcs.changesmanager.tree.features.PropertyFeature;
 import jetbrains.mps.ide.ui.smodel.ReferenceTreeNode;
 import org.jetbrains.mps.openapi.model.SReference;
+import jetbrains.mps.smodel.SNodePointer;
 import jetbrains.mps.ide.ui.smodel.PropertiesTreeNode;
 import jetbrains.mps.vcs.changesmanager.tree.features.PropertiesFeature;
 import jetbrains.mps.ide.ui.smodel.ReferencesTreeNode;
@@ -40,12 +38,9 @@ public class ProjectTreeFeatureExtractor implements TreeNodeFeatureExtractor {
     } else if (treeNode.getParent() instanceof MPSTreeNodeEx) {
       node = ((MPSTreeNodeEx) treeNode.getParent()).getSNode();
     }
-    SNodeReference nodePointer = null;
-    if (node != null) {
-      if (!(SNodeUtil.isAccessible(node, MPSModuleRepository.getInstance()))) {
-        return null;
-      }
-      nodePointer = new SNodePointer(node);
+    SNodeReference nodePointer = (node == null ? null : node.getReference());
+    if (nodePointer == null || nodePointer.getModelReference() == null) {
+      return null;
     }
     if (treeNode instanceof SNodeTreeNode || treeNode instanceof ConceptTreeNode) {
       return new NodeFeature(nodePointer);

plugins/vcs/common/source_gen/jetbrains/mps/vcs/changesmanager/NodeFileStatusMapping.java
@@ -37,7 +37,7 @@ public class NodeFileStatusMapping extends AbstractProjectComponent {
   private final CurrentDifferenceRegistry myRegistry;
   private final Map<SNodeReference, FileStatus> myFileStatusMap = MapSequence.fromMap(new HashMap<SNodeReference, FileStatus>());
   private final CurrentDifferenceListener myGlobalListener = new NodeFileStatusMapping.MyGlobalListener();
-  private final MPSProject myMPSProject;
+  protected final MPSProject myMPSProject;
   public NodeFileStatusMapping(MPSProject project, CurrentDifferenceRegistry registry) {
     super(project.getProject());
     myRegistry = registry;

plugins/vcs/common/source_gen/jetbrains/mps/vcs/changesmanager/editor/EditorHighlighter.java
@@ -14,10 +14,8 @@ import jetbrains.mps.vcs.changesmanager.CurrentDifference;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.project.Project;
 import jetbrains.mps.vcs.changesmanager.CurrentDifferenceRegistry;
-import jetbrains.mps.smodel.ModelAccess;
+import org.jetbrains.mps.openapi.module.SRepository;
 import org.jetbrains.mps.openapi.model.SNode;
-import org.jetbrains.mps.openapi.model.SNodeUtil;
-import jetbrains.mps.smodel.MPSModuleRepository;
 import org.jetbrains.mps.openapi.model.SModel;
 import org.jetbrains.mps.openapi.model.EditableSModel;
 import jetbrains.mps.vcs.diff.ChangeSet;
@@ -49,14 +47,15 @@ public class EditorHighlighter implements EditorMessageOwner {
 
     CurrentDifferenceRegistry.getInstance(project).getCommandQueue().runTask(new Runnable() {
       public void run() {
-        ModelAccess.instance().runReadAction(new Runnable() {
+        final SRepository repo = editorComponent.getEditorContext().getRepository();
+        repo.getModelAccess().runReadAction(new Runnable() {
           public void run() {
             synchronized (myDisposedLock) {
               if (myDisposed) {
                 return;
               }
-              final SNode editedNode = editorComponent.getEditedNode();
-              if (editedNode == null || !(SNodeUtil.isAccessible(editedNode, MPSModuleRepository.getInstance()))) {
+              final SNode editedNode = (editorComponent.getEditedNodePointer() == null ? null : editorComponent.getEditedNodePointer().resolve(repo));
+              if (editedNode == null) {
                 return;
               }
               final SModel model = editedNode.getModel();
@@ -109,10 +108,11 @@ public class EditorHighlighter implements EditorMessageOwner {
   private List<ChangeEditorMessage> createMessages(final ModelChange change) {
     final Wrappers._T<List<ChangeEditorMessage>> messages = new Wrappers._T<List<ChangeEditorMessage>>(null);
     if (!(change instanceof AddRootChange)) {
-      ModelAccess.instance().runReadAction(new Runnable() {
+      final SRepository repo = myEditorComponent.getEditorContext().getRepository();
+      repo.getModelAccess().runReadAction(new Runnable() {
         public void run() {
-          SNode editedNode = myEditorComponent.getEditedNode();
-          if (editedNode == null || !(SNodeUtil.isAccessible(editedNode, MPSModuleRepository.getInstance()))) {
+          SNode editedNode = (myEditorComponent.getEditedNodePointer() == null ? null : myEditorComponent.getEditedNodePointer().resolve(repo));
+          if (editedNode == null) {
             return;
           }
           SModel model = editedNode.getModel();
@@ -151,32 +151,28 @@ public class EditorHighlighter implements EditorMessageOwner {
     }
   }
   public void dispose() {
-    ModelAccess.instance().runReadAction(new Runnable() {
-      public void run() {
-        synchronized (myDisposedLock) {
-          myDisposed = true;
-          try {
-            synchronized (myChangesMessages) {
-              SetSequence.fromSet(MapSequence.fromMap(myChangesMessages).keySet()).toListSequence().visitAll(new IVisitor<ModelChange>() {
-                public void visit(ModelChange ch) {
-                  removeMessages(ch);
-                }
-              });
+    synchronized (myDisposedLock) {
+      myDisposed = true;
+      try {
+        synchronized (myChangesMessages) {
+          SetSequence.fromSet(MapSequence.fromMap(myChangesMessages).keySet()).toListSequence().visitAll(new IVisitor<ModelChange>() {
+            public void visit(ModelChange ch) {
+              removeMessages(ch);
             }
-            getHighlightManager().clearForOwner(EditorHighlighter.this);
-            if (myStripsPainter != null) {
-              getLeftEditorHighlighter().removeFoldingAreaPainter(myStripsPainter);
-              myStripsPainter.dispose();
-            }
-          } finally {
-            if (myCurrentDifference != null) {
-              myCurrentDifference.removeDifferenceListener(myListener);
-              myListener = null;
-            }
-          }
+          });
+        }
+        getHighlightManager().clearForOwner(this);
+        if (myStripsPainter != null) {
+          getLeftEditorHighlighter().removeFoldingAreaPainter(myStripsPainter);
+          myStripsPainter.dispose();
+        }
+      } finally {
+        if (myCurrentDifference != null) {
+          myCurrentDifference.removeDifferenceListener(myListener);
+          myListener = null;
         }
       }
-    });
+    }
   }
   @Nullable
   /*package*/ ChangeSet getChangeSet() {

plugins/vcs/source_gen/jetbrains/mps/vcs/changesmanager/roots/NodeFileStatusMappingExt.java
@@ -9,7 +9,6 @@ import com.intellij.openapi.vcs.FileStatusManager;
 import jetbrains.mps.workbench.nodesFs.MPSNodesVirtualFileSystem;
 import org.jetbrains.mps.openapi.model.SNodeReference;
 import org.jetbrains.mps.openapi.model.SNode;
-import jetbrains.mps.smodel.MPSModuleRepository;
 import jetbrains.mps.plugins.relations.RelationDescriptor;
 import jetbrains.mps.internal.collections.runtime.ListSequence;
 import jetbrains.mps.plugins.projectplugins.ProjectPluginManager;
@@ -21,7 +20,7 @@ public class NodeFileStatusMappingExt extends NodeFileStatusMapping {
   @Override
   protected void statusChanged(FileStatusManager fsm, MPSNodesVirtualFileSystem nvfs, SNodeReference nodePointer) {
     super.statusChanged(fsm, nvfs, nodePointer);
-    SNode currentNode = nodePointer.resolve(MPSModuleRepository.getInstance());
+    SNode currentNode = nodePointer.resolve(myMPSProject.getRepository());
     for (RelationDescriptor d : ListSequence.fromList(myProject.getComponent(ProjectPluginManager.class).getTabDescriptors())) {
       SNode baseNode = d.getBaseNode(currentNode);
       if (baseNode != null && baseNode != currentNode) {

plugins/vcs/source_gen/jetbrains/mps/vcs/changesmanager/tree/FeatureForestMap.java
@@ -2,6 +2,7 @@ package jetbrains.mps.vcs.changesmanager.tree;
 
 /*Generated by MPS */
 
+import org.jetbrains.mps.openapi.module.SRepository;
 import java.util.Map;
 import jetbrains.mps.vcs.changesmanager.tree.features.Feature;
 import jetbrains.mps.internal.collections.runtime.MapSequence;
@@ -27,17 +28,20 @@ import org.apache.log4j.LogManager;
  */
 public class FeatureForestMap<V> {
   private static boolean ourLoggingEnabled = "true".equals(System.getProperty("changesmanager.debug"));
-  private Map<Feature, V> myFeatureToValue = MapSequence.fromMap(new HashMap<Feature, V>());
-  private Map<Feature, Feature[]> myFeatureToAncestors = MapSequence.fromMap(new HashMap<Feature, Feature[]>());
-  private CounterMap<Feature> myFeaturesIsAncestorCounterMap = new CounterMap<Feature>(new FeatureForestMap.MyCounterMapHandler());
+  private final SRepository myRepo;
+  private final Map<Feature, V> myFeatureToValue = MapSequence.fromMap(new HashMap<Feature, V>());
+  private final Map<Feature, Feature[]> myFeatureToAncestors = MapSequence.fromMap(new HashMap<Feature, Feature[]>());
+  private final CounterMap<Feature> myFeaturesIsAncestorCounterMap = new CounterMap<Feature>(new FeatureForestMap.MyCounterMapHandler());
   private final List<FeatureForestMapListener> myListeners = ListSequence.fromList(new ArrayList<FeatureForestMapListener>());
-  public FeatureForestMap() {
+  public FeatureForestMap(SRepository repository) {
+    myRepo = repository;
   }
+
   public void put(@NotNull Feature feature, @NotNull V value) {
     if (MapSequence.fromMap(myFeatureToValue).containsKey(feature)) {
       log("Trying to put already present feature " + feature);
     }
-    Feature[] ancestors = feature.getAncestors();
+    Feature[] ancestors = feature.getAncestors(myRepo);
     Feature ancestor = Sequence.fromIterable(Sequence.fromArray(ancestors)).findFirst(new IWhereFilter<Feature>() {
       public boolean accept(Feature a) {
         return MapSequence.fromMap(myFeatureToValue).containsKey(a);
@@ -56,6 +60,7 @@ public class FeatureForestMap<V> {
     });
     fireFeatureStateChanged(feature);
   }
+
   public void removeKey(@NotNull Feature feature) {
     if (!(MapSequence.fromMap(myFeatureToValue).containsKey(feature))) {
       log("Trying to remove feature which is not added: " + feature);
@@ -72,47 +77,55 @@ public class FeatureForestMap<V> {
     });
     fireFeatureStateChanged(feature);
   }
+
   @Nullable
   public V get(@NotNull Feature feature) {
     return MapSequence.fromMap(myFeatureToValue).get(feature);
   }
+
   public boolean isAncestorOfAddedFeature(@NotNull Feature feature) {
     return !(myFeaturesIsAncestorCounterMap.isZero(feature));
   }
+
   @Nullable
   public Feature getAddedAncestor(@NotNull Feature feature) {
-    return Sequence.fromIterable(Sequence.fromArray(feature.getAncestors())).findFirst(new IWhereFilter<Feature>() {
+    return Sequence.fromIterable(Sequence.fromArray(feature.getAncestors(myRepo))).findFirst(new IWhereFilter<Feature>() {
       public boolean accept(Feature a) {
         return MapSequence.fromMap(myFeatureToValue).containsKey(a);
       }
     });
   }
+
   @Nullable
   public V getAddedAncestorValue(@NotNull Feature feature) {
     return MapSequence.fromMap(myFeatureToValue).get(getAddedAncestor(feature));
   }
+
   public void addListener(@NotNull FeatureForestMapListener listener) {
     synchronized (myListeners) {
       ListSequence.fromList(myListeners).addElement(listener);
     }
   }
+
   public void removeListener(@NotNull FeatureForestMapListener listener) {
     synchronized (myListeners) {
       ListSequence.fromList(myListeners).removeElement(listener);
     }
   }
+
   @NotNull
   private List<FeatureForestMapListener> copyListeners() {
     synchronized (myListeners) {
       return ListSequence.fromListWithValues(new ArrayList<FeatureForestMapListener>(), myListeners);
     }
   }
+
   private void fireFeatureStateChanged(@NotNull Feature feature) {
     for (FeatureForestMapListener listener : ListSequence.fromList(copyListeners())) {
       try {
         listener.featureStateChanged(feature);
       } catch (Throwable t) {
-        if (t instanceof InterruptedException || check_tcy57o_a0a0a0a0a0p(t) instanceof InterruptedException) {
+        if (t instanceof InterruptedException || check_tcy57o_a0a0a0a0a0ab(t) instanceof InterruptedException) {
           // ignore 
         } else {
           if (LOG.isEnabledFor(Level.ERROR)) {
@@ -122,9 +135,11 @@ public class FeatureForestMap<V> {
       }
     }
   }
+
   public static void setLoggingEnabled(boolean loggingEnabled) {
     ourLoggingEnabled = loggingEnabled;
   }
+
   private static void log(String msg) {
     if (ourLoggingEnabled) {
       if (LOG.isEnabledFor(Level.ERROR)) {
@@ -132,6 +147,7 @@ public class FeatureForestMap<V> {
       }
     }
   }
+
   private class MyCounterMapHandler implements CounterMap.CounterMapHandler<Feature> {
     public MyCounterMapHandler() {
     }
@@ -145,7 +161,7 @@ public class FeatureForestMap<V> {
     }
   }
   protected static Logger LOG = LogManager.getLogger(FeatureForestMap.class);
-  private static Throwable check_tcy57o_a0a0a0a0a0p(Throwable checkedDotOperand) {
+  private static Throwable check_tcy57o_a0a0a0a0a0ab(Throwable checkedDotOperand) {
     if (null != checkedDotOperand) {
       return checkedDotOperand.getCause();
     }

plugins/vcs/source_gen/jetbrains/mps/vcs/changesmanager/tree/FeatureForestMapSupport.java
@@ -10,6 +10,7 @@ import jetbrains.mps.internal.collections.runtime.MapSequence;
 import java.util.HashMap;
 import jetbrains.mps.vcs.changesmanager.CurrentDifferenceRegistry;
 import com.intellij.openapi.project.Project;
+import jetbrains.mps.project.MPSProject;
 import org.jetbrains.annotations.NotNull;
 import java.util.List;
 import jetbrains.mps.internal.collections.runtime.ListSequence;
@@ -33,12 +34,13 @@ import jetbrains.mps.internal.collections.runtime.Sequence;
 import jetbrains.mps.internal.collections.runtime.IVisitor;
 
 public class FeatureForestMapSupport extends AbstractProjectComponent {
-  private FeatureForestMap<ModelChange> myMap = new FeatureForestMap<ModelChange>();
+  private final FeatureForestMap<ModelChange> myMap;
   private Map<ModelChange, Feature[]> myChangeToFeaturesMap = MapSequence.fromMap(new HashMap<ModelChange, Feature[]>());
   private CurrentDifferenceRegistry myCurrentDifferenceRegistry;
   private FeatureForestMapSupport.MyListener myListener = new FeatureForestMapSupport.MyListener();
-  public FeatureForestMapSupport(Project project, CurrentDifferenceRegistry currentDifferenceRegistry) {
+  public FeatureForestMapSupport(Project project, MPSProject mpsProject, CurrentDifferenceRegistry currentDifferenceRegistry) {
     super(project);
+    myMap = new FeatureForestMap<ModelChange>(mpsProject.getRepository());
     myCurrentDifferenceRegistry = currentDifferenceRegistry;
   }
   @Override

plugins/vcs/source_gen/jetbrains/mps/vcs/changesmanager/tree/TreeHighlighter.java
@@ -243,7 +243,7 @@ public class TreeHighlighter implements TreeMessageOwner {
       public void run() {
         for (Feature anotherFeature : ListSequence.fromList(toCheck)) {
           // getAncestors might require (see NodeFeature) model read access, which shall not be under myFeaturesHolder lock 
-          if (Sequence.fromIterable(Sequence.fromArray(anotherFeature.getAncestors())).any(new IWhereFilter<Feature>() {
+          if (Sequence.fromIterable(Sequence.fromArray(anotherFeature.getAncestors(getProjectRepository()))).any(new IWhereFilter<Feature>() {
             public boolean accept(Feature a) {
               return feature.equals(a);
             }

plugins/vcs/source_gen/jetbrains/mps/vcs/changesmanager/tree/features/DeletedChildFeature.java
@@ -5,6 +5,7 @@ package jetbrains.mps.vcs.changesmanager.tree.features;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.mps.openapi.model.SNodeReference;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.mps.openapi.module.SRepository;
 
 public class DeletedChildFeature extends AbstractNodeFeature {
   private String myRole;
@@ -21,7 +22,7 @@ public class DeletedChildFeature extends AbstractNodeFeature {
   }
   @Nullable
   @Override
-  public Feature getParent() {
+  protected Feature getParent(SRepository repo) {
     return new NodeFeature(getNodePointer());
   }
 }

commit c74a25fe597d29c566df9e255a213293b3d3d4f1

MPS-26199 Git: unversioned models should be highlighted by red colour in project view
In method jetbrains.mps.ide.ui.tree.MPSTreeNode#updatePresentation(), if myTreeMessages != null, then from jetbrains.mps.ide.ui.tree.TreeMessage#getColor color for text is taken, which corresponds to one of the colors from com.intellij.openapi.vcs.FileStatus
This messages are set in method jetbrains.mps.vcs.changesmanager.tree.TreeHighlighter#rehighlightNode, which will be called, if in registration process for jetbrains.mps.ide.ui.tree.MPSTreeNode in method jetbrains.mps.vcs.changesmanager.tree.TreeHighlighter#registerNode there is jetbrains.mps.vcs.changesmanager.tree.features.Feature for this node.
And this was not true for Module (always was this way) and Model TreeNode.

For Models it was broken in commit ad50c6c1b9ed6bc8e3c4fca0434342f0296b455e:
SModelTreeNode does not implement MPSTreeNodeEx, as does not its getParent(), this means that node and nodePointer will be equals to null.
In that commit check was added, that nodePointer == null in which case null returned from method.
So code 'if (treeNode instanceof SModelTreeNode)' is unreachable, because its nodePointer is always == null.

plugins/vcs/source_gen/jetbrains/mps/vcs/changesmanager/tree/ProjectTreeFeatureExtractor.java
@@ -14,13 +14,13 @@ import jetbrains.mps.ide.ui.smodel.ConceptTreeNode;
 import jetbrains.mps.vcs.changesmanager.tree.features.NodeFeature;
 import jetbrains.mps.ide.ui.smodel.PropertyTreeNode;
 import jetbrains.mps.vcs.changesmanager.tree.features.PropertyFeature;
-import jetbrains.mps.ide.ui.smodel.ReferenceTreeNode;
-import org.jetbrains.mps.openapi.model.SReference;
-import jetbrains.mps.smodel.SNodePointer;
 import jetbrains.mps.ide.ui.smodel.PropertiesTreeNode;
 import jetbrains.mps.vcs.changesmanager.tree.features.PropertiesFeature;
 import jetbrains.mps.ide.ui.smodel.ReferencesTreeNode;
 import jetbrains.mps.vcs.changesmanager.tree.features.ReferencesFeature;
+import jetbrains.mps.ide.ui.smodel.ReferenceTreeNode;
+import org.jetbrains.mps.openapi.model.SReference;
+import jetbrains.mps.smodel.SNodePointer;
 import jetbrains.mps.ide.ui.tree.smodel.PackageNode;
 import jetbrains.mps.vcs.changesmanager.tree.features.VirtualPackageFeature;
 import jetbrains.mps.ide.ui.tree.smodel.SModelTreeNode;
@@ -39,26 +39,32 @@ public class ProjectTreeFeatureExtractor implements TreeNodeFeatureExtractor {
       node = ((MPSTreeNodeEx) treeNode.getParent()).getSNode();
     }
     SNodeReference nodePointer = (node == null ? null : node.getReference());
-    if (nodePointer == null || nodePointer.getModelReference() == null) {
-      return null;
+    if (nodePointer != null && nodePointer.getModelReference() != null) {
+      if (treeNode instanceof SNodeTreeNode || treeNode instanceof ConceptTreeNode) {
+        return new NodeFeature(nodePointer);
+      } else if (treeNode instanceof PropertyTreeNode && treeNode.getParent() instanceof MPSTreeNodeEx) {
+        return new PropertyFeature(nodePointer, ((PropertyTreeNode) treeNode).getProperty());
+      } else if (treeNode instanceof PropertiesTreeNode) {
+        return new PropertiesFeature(nodePointer);
+      } else if (treeNode instanceof ReferencesTreeNode) {
+        return new ReferencesFeature(nodePointer);
+      }
     }
-    if (treeNode instanceof SNodeTreeNode || treeNode instanceof ConceptTreeNode) {
-      return new NodeFeature(nodePointer);
-    } else if (treeNode instanceof PropertyTreeNode && treeNode.getParent() instanceof MPSTreeNodeEx) {
-      return new PropertyFeature(nodePointer, ((PropertyTreeNode) treeNode).getProperty());
-    } else if (treeNode instanceof ReferenceTreeNode) {
+
+    if (treeNode instanceof ReferenceTreeNode) {
       SReference ref = ((ReferenceTreeNode) treeNode).getRef();
       return new PropertyFeature(new SNodePointer(ref.getSourceNode()), ref.getRole());
-    } else if (treeNode instanceof PropertiesTreeNode) {
-      return new PropertiesFeature(nodePointer);
-    } else if (treeNode instanceof ReferencesTreeNode) {
-      return new ReferencesFeature(nodePointer);
-    } else if (treeNode instanceof PackageNode) {
+    }
+
+    if (treeNode instanceof PackageNode) {
       PackageNode pn = ((PackageNode) treeNode);
       return new VirtualPackageFeature(pn.getModelReference(), pn.getPackage());
-    } else if (treeNode instanceof SModelTreeNode) {
+    }
+
+    if (treeNode instanceof SModelTreeNode) {
       return new ModelFeature(((SModelTreeNode) treeNode).getModel().getReference());
     }
+
     return null;
   }
 }