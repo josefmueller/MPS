11ea74594c01490857464435d2d020631cdc59dd
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2017-08-21 16:24:42 +0300

    fix IDEA-177785 "configurable class name is not set" on mps 2017.2 on windows (MPS part of the issue mentioned by Kolja Dummann)

--- workbench/mps-platform/source/jetbrains/mps/plugins/prefs/BasePrefsPage.java
+++ workbench/mps-platform/source/jetbrains/mps/plugins/prefs/BasePrefsPage.java
@@ -41,6 +41,12 @@ public abstract class BasePrefsPage extends ConfigurableEP<Configurable> impleme
     return this;
   }
 
+  @Nullable
+  @Override
+  public Class<?> getConfigurableType() {
+    return null;
+  }
+
   @Override
   public Project getProject() {
     return myProject;
