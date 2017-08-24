e4565d7e4e6ede9c9c4fb71ff496d8460a02b984
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2017-08-24 11:34:39 +0200

    x-model: account for scenarios when output nodes of a ML doesn't belong to a latest transient model, assume any node registered as an ML output has to be in a final checkpoint model, report an error if not.
    (cherry picked from commit aca945c)

--- core/generator/source/jetbrains/mps/generator/impl/GeneratorLoggerAdapter.java
+++ core/generator/source/jetbrains/mps/generator/impl/GeneratorLoggerAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2015 JetBrains s.r.o.
+ * Copyright 2003-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -166,6 +166,10 @@ public class GeneratorLoggerAdapter implements IGeneratorLogger {
     for (Message m : messages) {
       addMessage(m);
     }
+  }
+
+  /*package*/ IMessageHandler getImplementation() {
+    return myMessageHandler;
   }
 
   interface MessageFactory {
