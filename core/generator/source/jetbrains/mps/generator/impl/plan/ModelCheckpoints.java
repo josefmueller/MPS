44dfd098954c47365d3110ff05c639890d0052ea
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2017-08-25 17:40:59 +0200

    x-model: with recorded information about origin CP in a CheckpointState, utilize this information to walk copies of reference target nodes in  checkpoints of a target model; drop a hack to address one particular case (input from origin model), which is handled in a generic way now.
    (cherry picked from commit e4c93ca)

--- core/generator/source/jetbrains/mps/generator/impl/plan/ModelCheckpoints.java
+++ core/generator/source/jetbrains/mps/generator/impl/plan/ModelCheckpoints.java
@@ -22,11 +22,14 @@ import jetbrains.mps.util.CollectionUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.mps.openapi.model.SModelReference;
+import org.jetbrains.mps.openapi.model.SNode;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -77,6 +80,84 @@ public class ModelCheckpoints {
       }
     }
     return null;
+  }
+
+  /**
+   * Two models are transformed independently and their generation plans not necessarily identical. The moment we'd like to get output
+   * for a certain target node, the node may belong to any earlier checkpoint of a target model, not necessarily immediately preceding one.
+   * Most common scenario is that target node (here, inputNode) points to original model, M2, which has gone through few transformation steps,
+   * CP1->CP2->CP3, and we'd like to get CP3 output given original M2 node. CP3 records its inputs as known at CP2, and we need to traverse
+   * back to CP1 and original model to find out matching node, and then traverse back, assuming node was merely copied between CP1 and CP2.
+   *
+   * FIXME In fact, need both CheckpointState for tp and SNode as return values, perhaps worth extracting into a separate class which keeps both
+   *       values as internal state, rather than obtaining it twice. Besides, separate class may hide assumption inputNode comes from same model
+   *       this object has been constructed for.
+   *
+   * @param tp checkpoint we'd like to get copied output for
+   * @param inputNode a node from one of previous checkpoints (including original model). This {@code {@link ModelCheckpoints} has to be
+   *        obtained for the node's model.
+   * @return outputNode a node in CP identified by {@code tp} representing a copy of supplied {@code inputNode}
+   */
+  @Nullable
+  private SNode findCopiedNode(@NotNull CheckpointIdentity tp, @NotNull SNode inputNode) {
+    // Given M1 and M2, where M1 hosts reference source and M2 is home for reference target, and cp sequence CP1->CP3 for M1, CP1->CP2->CP3 sequence for M2,
+    // we need to synchronize @CP3 provided M1 still points to M2@CP1, while M2@CP3 records nodes M2@CP2 as their inputs which do not match nodes of M2@CP1
+    // Therefore, we need to walk M2 CP sequence backwards up to CP1, match input and then re-construct transitions forward to CP3.
+    // FIXME perhaps, could make use of myPlanStep.getLastCheckpoint() not to get beyond that CP. However, need further investigation
+    //       regarding this, as there's no confidence why would both M1 and M2 share lastCheckpoint (CP3 might be the only one they share and synchronize at)
+    CheckpointIdentity cpId = tp;
+    ArrayDeque<CheckpointState> cpStateStack = new ArrayDeque<>();
+    SNode copiedOutput;
+    do {
+      CheckpointState cp = find(cpId);
+      if (cp == null) {
+        // can hardly do anything if we can't trace CPs
+        return null;
+      }
+      cpStateStack.push(cp);
+      copiedOutput = cp.getCopiedOutput(inputNode);
+      cpId = cp.getOriginCheckpoint();
+    } while (copiedOutput == null && cpId != null);
+    if (copiedOutput == null) {
+      // we traced all known CPs of M2 and didn't find any that has a copied output fro inputNode, alas
+      return null;
+    }
+    // at the top of the cpStateStack is CP state with the inputNode->copiedOutput record, discard as we already know copiedOutput node,
+    // i.e. CP1,CP2,CP3, and we discard CP1.
+    cpStateStack.pop();
+    // now, unwind the stack to get to output in 'targetPoint' (aka CP3)
+    while (!cpStateStack.isEmpty() && copiedOutput != null) {
+      CheckpointState cp = cpStateStack.pop();
+      copiedOutput = cp.getCopiedOutput(copiedOutput);
+    }
+    return copiedOutput;
+  }
+
+  /**
+   * With {@code inputNode} coming from one of CP previous to the supplied one, try to find a copy of an input node in immediately preceding CP
+   * and use it as a 'proper' input to retrieve values for the specified CP.
+   * @param tp
+   * @param inputNode
+   * @param withProperInput receives a {@link CheckpointState} identified by {@code tp}, and an input node that could be recorded in the state as an input
+   *                        for some mapping label
+   * @return first non-null result of {@code withProperInput} function, if any.
+   */
+  @Nullable
+  public SNode withProperInput(@NotNull CheckpointIdentity tp, @NotNull SNode inputNode, BiFunction<CheckpointState, SNode, SNode> withProperInput) {
+    CheckpointState cp = find(tp);
+    if (cp == null) {
+      // can hardly do anything if we can't trace CPs
+      return null;
+    }
+    // first, try if inputNode is proper for actual CP
+    SNode output = withProperInput.apply(cp, inputNode);
+    if (output == null && cp.getOriginCheckpoint() != null) {
+      // if not, and there's previous CP known,
+      // find output for inputNode at the previous CP, it will serve as proper input for the current CP.
+      SNode properInput = findCopiedNode(cp.getOriginCheckpoint(), inputNode);
+      output = properInput != null ? withProperInput.apply(cp, properInput) : null;
+    }
+    return output;
   }
 
   /*package*/ List<CheckpointIdentity> getKnownCheckpoints() {
