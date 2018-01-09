c0ae0434a93553a0832f1db4e5a5ccbd60a484b7
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2018-01-09 19:11:38 +0100

    Tests: drop code that used to populate/parameterise TransformationTest, now PushEnvironmentRunnerBuilder is responsible to pass Environment to the test.

--- plugins/mps-testing/languages/languageDesign/test/solutions/jetbrains.mps.lang.test.runtime/source_gen/jetbrains/mps/lang/test/runtime/TestRunner.java
+++ plugins/mps-testing/languages/languageDesign/test/solutions/jetbrains.mps.lang.test.runtime/source_gen/jetbrains/mps/lang/test/runtime/TestRunner.java
@@ -4,6 +4,11 @@ package jetbrains.mps.lang.test.runtime;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * 
+ * @deprecated There's no longer need to delegate initialization of a TransformationTest to external code. Instead, external code injects proper values (Environment) into the test at construction time. This class will be removed.
+ */
+@Deprecated
 public interface TestRunner {
   void initTest(final TransformationTest test, @NotNull String projectPath, String modelName, boolean reOpenProject) throws Exception;
 }
