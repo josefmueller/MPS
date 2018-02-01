0546c9d043bbb0029d1c22fdcb3bc528127e0081
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2018-02-01 11:38:59 +0100

    MPS-27391 NPE when node has no name.

--- workbench/mps-editor/source/jetbrains/mps/ide/editor/icons/NodeFileIconProvider.java
+++ workbench/mps-editor/source/jetbrains/mps/ide/editor/icons/NodeFileIconProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2016 JetBrains s.r.o.
+ * Copyright 2003-2018 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,13 +23,13 @@ import com.intellij.ui.DefaultIconDeferrer;
 import com.intellij.ui.IconDeferrer;
 import jetbrains.mps.fileTypes.MPSFileTypeFactory;
 import jetbrains.mps.ide.editor.MPSEditorUtil;
-import jetbrains.mps.ide.icons.IconManager;
+import jetbrains.mps.ide.icons.GlobalIconManager;
 import jetbrains.mps.ide.project.ProjectHelper;
 import jetbrains.mps.ide.vfs.VirtualFileUtils;
+import jetbrains.mps.nodefs.MPSNodeVirtualFile;
 import jetbrains.mps.smodel.SModelFileTracker;
 import jetbrains.mps.util.Computable;
 import jetbrains.mps.util.ModelComputeRunnable;
-import jetbrains.mps.nodefs.MPSNodeVirtualFile;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -42,6 +42,13 @@ import javax.swing.Icon;
  * evgeny, 12/25/11
  */
 public class NodeFileIconProvider implements FileIconProvider, ApplicationComponent {
+
+  private final GlobalIconManager myIconManager;
+
+  public NodeFileIconProvider(GlobalIconManager iconManager) {
+    myIconManager = iconManager;
+  }
+
   @Override
   @NonNls
   @NotNull
@@ -61,7 +68,7 @@ public class NodeFileIconProvider implements FileIconProvider, ApplicationCompon
   @Nullable
   public Icon getIcon(final VirtualFile file, int flags, final Project project) {
     if (file instanceof MPSNodeVirtualFile) {
-      final jetbrains.mps.project.Project mpsProject = ProjectHelper.toMPSProject(project);
+      final jetbrains.mps.project.Project mpsProject = ProjectHelper.fromIdeaProject(project);
       if (mpsProject == null) {
         return null;
       }
@@ -72,13 +79,13 @@ public class NodeFileIconProvider implements FileIconProvider, ApplicationCompon
           if (IconDeferrer.getInstance() instanceof DefaultIconDeferrer) {
             SNode node = MPSEditorUtil.getCurrentEditedNode(project, nodeFile);
             if (node != null) {
-              return IconManager.getIconFor(node);
+              return myIconManager.getIconFor(node);
             }
             // TODO: get current empty tab component in MPSEditorUtil by using ((TabbedEditor) nodeEditor).myTabsComponent.getCurrentTabAspect()[.getIcon]
           }
           SNode node = nodeFile.getNode();
           if (node != null) {
-            return IconManager.getIconFor(node);
+            return myIconManager.getIconFor(node);
           }
           return null;
         }
@@ -92,11 +99,14 @@ public class NodeFileIconProvider implements FileIconProvider, ApplicationCompon
         @Override
         public Icon compute() {
           SModel descr = SModelFileTracker.getInstance(mpsProject.getRepository()).findModel(VirtualFileUtils.toIFile(file.getParent()));
-          if(descr == null) return null;
+          if(descr == null) {
+            return null;
+          }
 
+          String nameWithoutExtension = file.getNameWithoutExtension();
           for (SNode node : descr.getRootNodes()) {
-            if(node.getName().equals(file.getNameWithoutExtension())) {
-              return IconManager.getIconFor(node);
+            if(nameWithoutExtension.equals(node.getName())) {
+              return myIconManager.getIconFor(node);
             }
           }
           return null;
