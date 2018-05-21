b4ebf8e13b69e29f3a656710d6bed190f954df9f
Victor.Matchenko <Victor.Matchenko@jetbrains.com>
2018-05-21 14:30:39 +0200

    platform 182.2371: DefaultCodeStyleFacade was deleted
    Extend parent class instead. Copy methods implementations from deleted DefaultCodeStyleFacade.
    See Idea Community commit 2966eac93829d5c20a6d0da89c265399acdb5ea4

--- workbench/mps-workbench/source/jetbrains/mps/codeStyle/MPSCodeStyleFacade.java
+++ workbench/mps-workbench/source/jetbrains/mps/codeStyle/MPSCodeStyleFacade.java
@@ -15,13 +15,19 @@
  */
 package jetbrains.mps.codeStyle;
 
-import com.intellij.codeStyle.DefaultCodeStyleFacade;
+import com.intellij.codeStyle.CodeStyleFacade;
+import com.intellij.lang.Language;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.project.Project;
 import jetbrains.mps.baseLanguage.util.CodeStyleSettings;
 import jetbrains.mps.baseLanguage.util.CodeStyleSettingsRegistry;
 import jetbrains.mps.ide.project.ProjectHelper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class MPSCodeStyleFacade extends DefaultCodeStyleFacade {
+public class MPSCodeStyleFacade extends CodeStyleFacade {
   private final Project myProject;
 
   public MPSCodeStyleFacade() {
@@ -33,8 +39,44 @@ public class MPSCodeStyleFacade extends DefaultCodeStyleFacade {
   }
 
   @Override
+  public int getIndentSize(FileType fileType) {
+    return 4;
+  }
+
+  @Nullable
+  @Override
+  public String getLineIndent(@NotNull Document document, int offset) {
+    return null;
+  }
+
+  @Override
   public String getLineSeparator() {
     final CodeStyleSettings settings = CodeStyleSettingsRegistry.getSettings(ProjectHelper.fromIdeaProject(myProject));
     return settings != null ? settings.getLineSeparatorValue() : System.lineSeparator();
+  }
+
+  @Override
+  public int getRightMargin(Language language) {
+    return 80;
+  }
+
+  @Override
+  public int getTabSize(FileType fileType) {
+    return 4;
+  }
+
+  @Override
+  public boolean isSmartTabs(FileType fileType) {
+    return false;
+  }
+
+  @Override
+  public boolean isUnsuitableCodeStyleConfigurable(Configurable c) {
+    return false;
+  }
+
+  @Override
+  public boolean useTabCharacter(FileType fileType) {
+    return false;
   }
 }
